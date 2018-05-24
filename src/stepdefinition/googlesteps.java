4c42226fa92ffc2540e0dfa4cd072d5d1f00a98c
Brijendra <bps.brijendra@gmail.com>
2018-05-24 14:01:28 +0530

    masterthought reporting added

--- src/stepdefinition/googlesteps.java
+++ src/stepdefinition/googlesteps.java
@@ -1,5 +1,7 @@
 package stepdefinition;
 
+import static org.junit.Assert.assertTrue;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -16,5 +18,6 @@ public class googlesteps {
 	public void i_read_the_googles_news_headlines() throws Throwable {
 	    System.out.println("Goole news is read");
 	    new GooglePage().writescenario();
+	    assertTrue(false);
 	}
 }
